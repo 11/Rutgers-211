4d7460fa7fcdc91a8e410fbdefdf32f12fac79fb
11 <drudolph914@gmail.com>
2016-10-21 20:35:06 -0400

    final build

--- assignment_two/runner.c
+++ assignment_two/runner.c
@@ -59,7 +59,7 @@ int main(int argc, char** argv)
 	//debug purposes
 	//table.printlist(table.hashlist, table.size);	
 
-	printf("count: %d\n", count_diff);
+	printf("%d\n", count_diff);
 	return 0;
 } 
 
